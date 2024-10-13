7ba2797ad4002f17ec63ec9f241b7a030280c5b3
PeyHunter <Nando.goat@gmail.com>
2024-10-13 22:37:37 +0000

    Creating a simple tax calculation system
    working on the basics of IntelliJ, I am trying to understand different concept and working with this project to learn.

---
+++ src/main/java/org/example/MomsTal.java
@@ -0,0 +1,52 @@
+
+/*public class MomsTal() {
+
+    public int moms;
+    public int subTotal;
+    public int total;
+
+    public MomsTal() {}
+
+    public MomsTal(int m, int s, int t) {
+        moms = m;
+        subTotal = s;
+        total = t;
+    }
+
+    public int getMoms() {
+        return moms;
+    }
+
+    public int getSubTotal() {
+        return subTotal;
+    }
+
+    public int getTotal() {
+        return moms;
+    }
+
+
+    public int udregnMomsAfSubTotal(int s) {
+        subTotal = s;
+        if (s > 0) {
+            return (s * 1.25);
+        }
+
+        return -1;
+    }
+
+
+
+
+}*/
+
+
+
+ /*public int udregnSubTotalAfTotal(int t) {
+        total = t;
+        if (t > 0) {
+            return (t * 1.25);
+        }
+
+        return -1;
+    }*/
---
+++ src/main/java/org/example/MomsTaligen.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class MomsTaligen {
+
+    private int moms;
+    private int subTotal;
+    private int total;
+
+    public MomsTaligen(){}
+
+
+    public int moms(int m)
+    {
+        return (int) (m * 20);
+    }
+
+}
